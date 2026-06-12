8ae23a9e15ec11a8d66dc5822afa6600ab32fa3d
Marisol Flores <mariflores5877@gmail.com>
2026-06-11 19:57:49 -0500

    feat: add Section class with constructor, renderItems, and addItem methods

---
+++ components/Section.js
@@ -0,0 +1,17 @@
+class Section {
+  constructor({ items, renderer, containerSelector }) {
+    this._items = items;
+    this._renderer = renderer;
+    this._containter = document.querySelector(containerSelector);
+  }
+
+  renderItems() {
+    this._items.forEach((item) => this._renderer(item));
+  }
+
+  addItem(element) {
+    this._containter.prepend(element);
+  }
+}
+
+export default Section;
