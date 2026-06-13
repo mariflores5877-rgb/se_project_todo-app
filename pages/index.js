31fc5a1b9279bfcdfa38ee55f77a9c80f357a600
Marisol Flores <mariflores5877@gmail.com>
2026-06-13 14:45:56 -0500

    refactor: replace vanilla model functions with PopupWithForm instance in index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 import { initialTodos, validationConfig } from "../utils/constants.js";
 import { Todo } from "../components/Todo.js";
 import { FormValidator } from "../components/FormValidator.js";
+import PopupWithForm from "../components/PopupWithForm.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
@@ -21,42 +22,19 @@ addTodoButton.addEventListener("click", () => {
   openModal(addTodoPopup);
 });
 
-addTodoCloseBtn.addEventListener("click", () => {
-  closeModal(addTodoPopup);
-});
-
-function handleEscape(evt) {
-  if (evt.key === "Escape") {
-    const openedPopup = document.querySelector(".popup_visible");
-    closeModal(openedPopup);
-  }
-}
-
-function openModal(modal) {
-  modal.classList.add("popup_visible");
-  document.addEventListener("keydown", handleEscape);
-}
-
-function closeModal(modal) {
-  modal.classList.remove("popup_visible");
-  document.removeEventListener("keydown", handleEscape);
-}
-
-addTodoForm.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  const name = evt.target.name.value;
-  const dateInput = evt.target.date.value;
-
-  // Create a date object and adjust for timezone
+const addTodoPopupForm = new PopupWithForm("#add-todo-form", (inputValues) => {
+  const name = inputValues.name;
+  const dateInput = inputValues.date;
   const date = new Date(dateInput);
   date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
-
   const id = uuidv4();
   const values = { name, date, id };
   renderTodo(values);
   formValidator.resetValidation();
-  closeModal(addTodoPopup);
+  addTodoPopupForm.close();
 });
+
+addTodoPopupForm.setEventListeners();
 
 initialTodos.forEach((item) => {
   renderTodo(item);
