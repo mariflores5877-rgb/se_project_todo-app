054d3f60a22e1ad4a0ab0f7cc0b4def683921eed
Marisol Flores <mariflores5877@gmail.com>
2026-06-13 14:44:42 -0500

    feat: add PopupWithForm class extending Popup with form submission handling

---
+++ components/PopupWithForm.js
@@ -0,0 +1,28 @@
+import { Popup } from "./Popup.js";
+
+class PopupWithForm extends Popup {
+  constructor(popupSelector, handleFormSubmit) {
+    super(popupSelector);
+    this._handleFormSubmit = handleFormSubmit;
+    this._form = this._popup.querySelector("form");
+  }
+
+  _getInputValues() {
+    const inputList = this._form.querySelectorAll("input");
+    const inputValues = {};
+    inputList.forEach((input) => {
+      inputValues[input.name] = input.value;
+    });
+    return inputValues;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._form.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._handleFormSubmit(this._getInputValues());
+    });
+  }
+}
+
+export default PopupWithForm;
