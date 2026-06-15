1893a7fb39563fa51b6f4d4cfc9e41f06d095109
Marisol Flores <mariflores5877@gmail.com>
2026-06-14 21:41:02 -0500

    feat; add TodoCounter class with completed and total tracking

---
+++ components/TodoCounter.js
@@ -0,0 +1,31 @@
+class TodoCounter {
+  constructor(todos, selector) {
+    this._element = document.querySelector(selector);
+    this._completed = todos.filter((item) => item.done).length;
+    this._total = todos.length;
+  }
+
+  updateCompleted = (increment) => {
+    if (increment) {
+      this._completed++;
+    } else {
+      this._completed--;
+    }
+    this._updateText();
+  };
+
+  updateTotal = (increment) => {
+    if (increment) {
+      this._total++;
+    } else {
+      this._total--;
+    }
+    this._updateText();
+  };
+
+  _updateText() {
+    this._element.textContent = `Showing ${this._completed} out of ${this._total} completed`;
+  }
+}
+
+export default TodoCounter;
